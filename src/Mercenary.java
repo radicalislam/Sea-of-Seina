7d85d1df5a5107817f6616d24d24cad6fc4089c2
PizzaHutPersonalPanPizza <50241172+PizzaHutPersonalPanPizza@users.noreply.github.com>
2019-05-29 07:41:51 -0700

    Removed files

--- src/Mercenary.java
+++
@@ -1,8 +0,0 @@
-
-public class Mercenary extends Ship {
-
-	public Mercenary() {
-		super(60, 50, 15, 15, 100, 35);
-	}
-
-}
--- src/Pirate.java
+++
@@ -1,8 +0,0 @@
-
-public class Pirate extends Ship {
-
-	public Pirate() {
-		super(90, 30, 50, 6, 90, 40);
-	}
-
-}
