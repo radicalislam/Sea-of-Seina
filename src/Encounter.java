34dd8dccbcc34e51c09c512c9f140b6f6eca768a
Sho Nakata <1080881@SUHSD.NET>
2019-05-29 07:48:23 -0700

    Seina is ok i guess

--- src/Encounter.java
+++ src/Encounter.java
@@ -8,25 +8,18 @@ public class Encounter extends Game
 {
 	// instance variables - replace the example below with your own
 	private int x;
-	
+	private int eventCount;
 	/**
 	* Constructor for objects of class Encounter
 	*/
 	public Encounter()
 	{
 		// initialise instance variables
-		x = 0;
+		double Percentile = Math.random();
+		if(Percentile > 0 && Percentile < 0.05) /*Merchant Ship*/
+		{
+			System.out.println
+		}
 	}
 	
 	
-	public int sampleMethod(int y)
-	{
-		// put your code here
-		return x + y;
-	}
-	
-	private void pressEnterToContinue()
-	{
-		String startGame = readLine("Press Enter to Continue");
-    }
-}
