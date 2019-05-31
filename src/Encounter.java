0db15bb82d564ffc5515f32aacbaf513c4945eea
EduardoBeaner <50919177+EduardoBeaner@users.noreply.github.com>
2019-05-30 18:18:28 -0700

    German techology number one

--- src/Encounter.java
+++ src/Encounter.java
@@ -126,13 +126,115 @@ public class Encounter extends Game
 				pressEnterToContinue();
 				System.out.println("Sho: It is the Endeavor...");
 				pressEnterToContinue();
+				if(ship.getBoat() == 'r') /*If Royal Ship*/
+					{
+						System.out.print("Your ship just sails by along with a couple of waves and cheering.");
+					}	
+				if(ship.getBoat() == '$')
+					{
+						System.out.print("What would you do?");
+						System.out.print("A: Royals:'We shall ask for a tenth of your gold, as we are permitting the sails of our sea.");
+						System.out.print("B: Sho:'Money beats you fool's lives in terms of value!'");
+						String option = sc.nextLine();
+						eventCount++;
+					}	
+				else
+					{
+						System.out.print("Sho: Prepare the Cannons!"); /*Engage in battle*/
+						eventCount++;
+					}	
+
+			}
+		if(Percentile > 0.70 && Percentile <= 0.80) /*Wrecked Ship*/
+			{
+				System.out.println("You come across something that once used to be a merchant's ship");
+				pressEnterToContinue();
 				System.out.print("What would you do?");
-				
-				System.out.print("A: Join the fun");
-				System.out.print("B: 'You animals. This ship is a drug free vessel'");
+				System.out.print("A: Board and Loot");
+				System.out.print("B: Ignore");
 				String option = sc.nextLine();
 				eventCount++;
-			}	
+			}
+		if(Percentile > 0.80 && Percentile <= 0.82) /*Doby Mick Whale*/
+			{
+				System.out.println("The ship jerks to the right. It is as if something scraped against it.");
+				pressEnterToContinue();
+				System.out.println("Sho: NANI?");
+				pressEnterToContinue();
+				System.out.println("Right next to your ship, a huge whale jumps out and dives back in the water");
+				pressEnterToContinue();
+				System.out.print("What would you do?");
+				System.out.print("A: Sho: 'Get the cannons ready!'");
+				System.out.print("B: Sho: 'Full Sail Ahead!!!'");
+				System.out.print("C: Grab alot of bread, and try to feed it");
+				String option = sc.nextLine();
+				eventCount++;
+			}
+		if(Percentile > 0.82 && Percentile <= 0.83)/*Flying Dutchman*/
+			{
+				System.out.println("At night, in the far distance a green light steadily approaches.");
+				pressEnterToContinue();
+				System.out.println("As it gets closer, you realize it is a ship moving at an incredible speed.");
+				pressEnterToContinue();
+				System.out.println("The side of the ship is written as 'The Flying Dutchman'");
+				pressEnterToContinue();
+				System.out.print("What would you do?");
+				System.out.print("A: Enter Combat");
+				System.out.print("B: Offer your crew for safety");
+				String option = sc.nextLine();
+				eventCount++;
+			}
+		if(Percentile > 0.83 && Percentile <= 0.84)/*Kraken*/
+			{
+				System.out.println("The ship jerks to the right. It is as if something scraped against it.");
+				pressEnterToContinue();
+				System.out.println("Sho: NANI?");
+				pressEnterToContinue();
+				System.out.println("Right next to your ship, a huge tentacle squirms out of the water.");
+				pressEnterToContinue();
+				//Put here the combat
+				eventCount++;
+			}
+		if(Percentile > 0.84 && Percentile <= 0.94)/*Vortex*/
+		{
+			System.out.println("Crew: Captain! The ship's way is blocked by a vortex!");
+			pressEnterToContinue();
+			System.out.print("What would you do?");
+			System.out.print("A: Sho: 'No matter! Full Sail Ahead!'");
+			System.out.print("B: Avoid the vortex");
+			String option = sc.nextLine();
+			eventCount++;
+		}
+		if(Percentile > 0.94 && Percentile <= 0.99)/*Stowaway*/
+		{
+			System.out.println("Crew: Captain! We have found this kid snucking in our ship!");
+			pressEnterToContinue();
+			System.out.print("What would you do?");
+			System.out.print("A: Throw him overboard");
+			System.out.print("B: Make him part of your crew");
+			String option = sc.nextLine();
+			eventCount++;
+		}
+		if(Percentile > 0.99 && Percentile <= 0.1)/*GERMAN SHIP*/
+		{
+			System.out.println("Crew: 'YO WTF! CAPTAIN THERE IS A SHIP MADE OF METAL ON THE SEA?!'");
+			pressEnterToContinue();
+			System.out.println("Sho: 'WHAT... IS... THAT?!'");
+			pressEnterToContinue();
+			System.out.println("A man on the 'metal' ship comes out and yells.");
+			pressEnterToContinue();
+			System.out.println("Stroheim: 'BEHOLD OUR GERMAN SHIP! GERMAN TECHNOLOGY IS NUMBER ONE!!!'");
+			pressEnterToContinue();
+			System.out.print("What would you do?");
+			System.out.print("A: Enter Combat"); //get onesidedly destroyed no matter what
+			System.out.print("B: Praise Stroheim and have him modify your ship");//expensive af
+			System.out.print("C: Bow down and praise him. Your grandma told you to never mess with crazy people with crazy inventions.");
+			String option = sc.nextLine();
+			eventCount++;
+		}
+		
+
+	
 	}
 	
 		
